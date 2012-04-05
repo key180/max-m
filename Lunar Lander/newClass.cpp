948730e8089d0ba75ad1887bbb15c92162088a2d
maxwell.mckinnon <maxwell.mckinnon@80b8411f-c761-477a-947b-7fdcab2cf58b>
2012-04-05 00:58:50 +0000

    don't mess this one up xd

---
+++ Lunar Lander/newClass.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   newClass.cpp
+ * Author: Paul
+ * 
+ * Created on April 4, 2012, 4:21 PM
+ */
+
+#include "newClass.h"
+
+
+newClass::newClass() {
+}
+
+newClass::newClass(const newClass& orig) {
+}
+
+newClass::~newClass() {
+}
+
+void newClass::test(){
+    
+}
