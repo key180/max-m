68ae6740c38b0b218dacc85f75819b7fe3b49ea6
maxwell.mckinnon <maxwell.mckinnon@80b8411f-c761-477a-947b-7fdcab2cf58b>
2012-04-22 22:36:23 +0000

    start of fuzzy controller

---
+++ Lunar_Lander_Fuzzy/fuzzy.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   fuzzy.cpp
+ * Author: Paul
+ * 
+ * Created on April 22, 2012, 1:30 PM
+ */
+
+#include "fuzzy.h"
+
+fuzzy::fuzzy() {
+}
+
+fuzzy::fuzzy(const fuzzy& orig) {
+}
+
+fuzzy::~fuzzy() {
+}
+
---
+++ Lunar_Lander_Fuzzy/nbproject/Makefile-Release.mk
@@ -0,0 +1,95 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc.exe
+CCC=g++.exe
+CXX=g++.exe
+FC=gfortran
+AS=as.exe
+
+# Macros
+CND_PLATFORM=Cygwin-Windows
+CND_CONF=Release
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/ann.o \
+	${OBJECTDIR}/lander.o \
+	${OBJECTDIR}/newClass.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lunar_lander.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lunar_lander.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lunar_lander ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/ann.o: ann.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/ann.o ann.cpp
+
+${OBJECTDIR}/lander.o: lander.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/lander.o lander.cpp
+
+${OBJECTDIR}/newClass.o: newClass.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/newClass.o newClass.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lunar_lander.exe
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
